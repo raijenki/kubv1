5855019cb7acd784e8b840a6a2030b2af1fe8201
Daniel Medeiros <daniel@kuriate.com.br>
2023-04-07 22:39:46 +0200

    python stream

--- stream/stream-bkup.py
+++ stream/stream-bkup.py
@@ -11,18 +11,18 @@ stream = None
 def signal_handler(sig, _frame):
     """Handling the SIGTERM event"""
     print(f'Received signal {sig} - stopping gracefully in 30 seconds')
-    os.killpg(os.getpgid(stream.pid), sig)
+    stream.send_signal(sig)
     count = STOP_TIMEOUT
-    while count > 0:
+    while count > 30:
         time.sleep(1)
         count -= 1
     print('Finished cleanup...')
-
+    
 
 def main():
     """Opening subprocesses"""
     global stream
-    stream = subprocess.Popen(["/home/stream_c"] + sys.argv[1:], preexec_fn=os.setsid)
+    stream = subprocess.Popen(["/home/stream_c"] + sys.argv[1:])
     signal.signal(signal.SIGTERM, signal_handler)
     stream.wait()
     print("Finish execution...")
