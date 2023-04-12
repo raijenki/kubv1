6df0cfbe1b8a496b4d6cb262d4d910aa2a06159d
Daniel Medeiros <daniel@kuriate.com.br>
2023-04-12 13:55:00 +0200

    mod launcher

--- cm1/launcher.py
+++ cm1/launcher.py
@@ -4,10 +4,14 @@ import signal
 import os
 import sys
 import subprocess
+import shlex
 from multiprocessing import Process
 
-STOP_TIMEOUT = 10
-stream = None
+STOP_TIMEOUT = 20
+app = None
+app_ssh = None
+MASTER_CMD = "mpiexec --allow-run-as-root -wdir /home/hpc-tests/cm1/ --host ${MPI_HOST} -np 4 /home/hpc-tests/cm1/cm1.exe"
+WORKER_CMD = "/usr/sbin/sshd -D"
 
 def signal_handler(sig, _frame):
     """Handling the SIGTERM event"""
@@ -22,23 +26,28 @@ def signal_handler(sig, _frame):
 
 def main_worker():
     """Opening subprocesses"""
-    global stream
-    cm1 = subprocess.Popen(["/usr/sbin/sshd"], args=["-D"], preexec_fn=os.setsid)
+    global app
+    app = subprocess.Popen(shlex.split(WORKER_CMD), preexec_fn=os.setsid)
     signal.signal(signal.SIGTERM, signal_handler)
-    cm1.wait()
+    app.wait()
     print("Finish execution...")
 
 def main_master():
+    global app
     """Opening subprocesses"""
-    global stream
-    stream = subprocess.Popen(["/home/stream_c"] + sys.argv[1:], preexec_fn=os.setsid)
+    ssh_hosts = open("/etc/volcano/mpiworker.host")
+    MPI_HOST = ','.join(line.strip() for line in ssh_hosts)
+    os.environ["MPI_HOST"] = MPI_HOST
+    
+    app_ssh = subprocess.Popen("/usr/sbin/sshd", preexec_fn=os.setsid)
+    app = subprocess.Popen(shlex.split(MASTER_CMD), preexec_fn=os.setsid)
     signal.signal(signal.SIGTERM, signal_handler)
-    stream.wait()
+    app.wait()
     print("Finish execution...")
 
 if __name__ == "__main__":
+    """Defines whether we should follow the master or work router"""
     f = open('/etc/hostname')
-
     podname = f.read()
     if "worker" in podname: 
         main_worker()
