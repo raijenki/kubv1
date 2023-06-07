f5c34eb969031e159d2717c1efa766ec785443e7
Daniel Medeiros <daniel@kuriate.com.br>
2023-06-07 16:54:14 +0200

    mpi_host

--- stream-mpi/launcher.py
+++ stream-mpi/launcher.py
@@ -167,9 +167,9 @@ def start_mpi(extra_args=None):
     os.environ["MPI_HOST"] = MPI_HOST
 
     if extra_args is None: # important for gmx
-        MASTER_CMD = "mpiexec --allow-run-as-root -wdir /home/hpc-tests/stream-mpi/ --host " +  str(MPI_HOST) + " -np " + str(getNumberOfRanks()) + " stream_mpi 100000000 600"
+        MASTER_CMD = "mpiexec --allow-run-as-root -wdir /home/hpc-tests/stream-mpi/ --host " +  str(MPI_HOST) + " -np " + str(getNumberOfRanks()) + " stream_mpi 100000000 1000"
     else:
-        MASTER_CMD = "mpiexec --allow-run-as-root -wdir /home/hpc-tests/stream-mpi/ --host " +  str(MPI_HOST) + " -np " + str(getNumberOfRanks()) + " stream_mpi 100000000 600 " + str(extra_args)
+        MASTER_CMD = "mpiexec --allow-run-as-root -wdir /home/hpc-tests/stream-mpi/ --host " +  str(MPI_HOST) + " -np " + str(getNumberOfRanks()) + " stream_mpi 100000000 1000 " + str(extra_args)
 
     #app = subprocess.Popen(shlex.split(MASTER_CMD), start_new_session=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     app = subprocess.Popen(shlex.split(MASTER_CMD), start_new_session=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
