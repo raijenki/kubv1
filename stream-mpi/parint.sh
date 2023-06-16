c7df52205a1a818f96b5ac7c3ad0fc403088053f
Daniel Medeiros <daniel@kuriate.com.br>
2023-06-16 20:56:21 +0200

    auto update nloop

--- stream-mpi/parint.sh
+++ stream-mpi/parint.sh
@@ -7,18 +7,18 @@ for val in "${values[@]}"
 do
 	sed -i "s/#define NLOOP [0-9]\+/#define NLOOP $val/" stream_mpi.c 
 	git add .
-	git commit -m "update nloop"
+	git commit -m "auto update nloop"
 	git push
 	docker build . --tag=raijenki/mpik8s:smpi --no-cache
 	docker push raijenki/mpik8s:smpi
 	for i in 1 2 3
 	do
-		echo "STARTING $val - Trial $i" >> parint_6ranks.txt 
+		echo "STARTING $val - Trial $i" >> parint_4ranks.txt 
 		kubectl create -f smpi-vanilla.yaml
 		sleep 900
-		kubectl describe job.batch.volcano.sh >> parint_6ranks.txt 
+		kubectl describe job.batch.volcano.sh >> parint_4ranks.txt 
 		kubectl delete -f smpi-vanilla.yaml
 		sleep 10
-		echo "FINISHED" >> parint_6ranks.txt
+		echo "FINISHED" >> parint_4ranks.txt
 	done
 done
