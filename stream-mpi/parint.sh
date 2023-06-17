0e5e5fb917a3a6edaaad5fed5cd2cf664f841876
Daniel Medeiros <daniel@kuriate.com.br>
2023-06-17 18:51:28 +0200

    prep for 2 to 4 ranks

--- stream-mpi/parint.sh
+++ stream-mpi/parint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #values=(8 16 32 48 64)
-values=(8 16 32 48 64)
-
+values=(16 64)
+scenarios=(90 150 210 320 530 740) 
 for val in "${values[@]}"
 do
 	sed -i "s/#define NLOOP [0-9]\+/#define NLOOP $val/" stream_mpi.c 
@@ -11,14 +11,37 @@ do
 	git push
 	docker build . --tag=raijenki/mpik8s:smpi --no-cache
 	docker push raijenki/mpik8s:smpi
-	for i in 1 2 3
+	for scen in "${scenarios[@]}"
 	do
-		echo "STARTING $val - Trial $i" >> parint_4ranks.txt 
-		kubectl create -f smpi-vanilla.yaml
-		sleep 900
-		kubectl describe job.batch.volcano.sh >> parint_4ranks.txt 
-		kubectl delete -f smpi-vanilla.yaml
+	if [ $scen -lt 300 ]
+	then
+		for i in 1 2 3
+		do
+		echo "STARTING $val - Trial $i" >> parint_2to4ranks_16.txt 
+		kubectl create -f smpi.yaml
+		sleep $scenarios
+		kubectl create -f scheduler.yaml
+		sleep 300
+		kubectl describe job.batch.volcano.sh >> parint_2to4ranks_16.txt 
+		kubectl delete -f smpi.yaml -f scheduler.yaml
 		sleep 10
-		echo "FINISHED" >> parint_4ranks.txt
+		echo "FINISHED" >> parint_2to4ranks_16.txt
+		done
+	fi
+	if [ $scen -gt 300 ]
+	then
+		for i in 1 2 3
+		do
+		echo "STARTING $val - Trial $i" >> parint_2to4ranks_64.txt 
+		kubectl create -f smpi.yaml
+		sleep $scenarios
+		kubectl create -f scheduler.yaml
+		sleep 900
+		kubectl describe job.batch.volcano.sh >> parint_2to4ranks_64.txt 
+		kubectl delete -f smpi.yaml -f scheduler.yaml
+		sleep 10
+		echo "FINISHED" >> parint_2to4ranks_64.txt
+		done
+	fi
 	done
 done
