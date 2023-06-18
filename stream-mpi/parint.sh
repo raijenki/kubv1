767971a543720a1a6a2c4e1491ea6ec1d22db2c1
Daniel Medeiros <daniel@kuriate.com.br>
2023-06-18 10:28:10 +0200

    auto update nloop

--- stream-mpi/parint.sh
+++ stream-mpi/parint.sh
@@ -2,7 +2,8 @@
 
 #values=(8 16 32 48 64)
 values=(16 64)
-scenarios=(90 150 210 320 530 740) 
+#scenarios=(90 150 210 320 530 740) 
+scenarios=(34 58 80 113 190 262)
 
 for val in "${values[@]}"
 do
@@ -14,34 +15,34 @@ do
 	docker push raijenki/mpik8s:smpi
 	for scen in "${scenarios[@]}"
 	do
-	if [ $scen -lt 300 ]
+	if [ $scen -lt 100 ]
 	then
 		for i in 1 2 3
 		do
-		echo "STARTING $val - Trial $i - Base $scen" >> parint_2to6ranks_16.txt 
+		echo "STARTING $val - Trial $i - Base $scen" >> parint_4to6ranks_16.txt 
 		kubectl create -f smpi.yaml
 		sleep $scenarios
 		kubectl create -f scheduler.yaml
-		sleep 900
-		kubectl describe job.batch.volcano.sh >> parint_2to6ranks_16.txt 
+		sleep 200
+		kubectl describe job.batch.volcano.sh >> parint_4to6ranks_16.txt 
 		kubectl delete -f smpi.yaml -f scheduler.yaml
 		sleep 10
-		echo "FINISHED" >> parint_2to6ranks_16.txt
+		echo "FINISHED" >> parint_4to6ranks_16.txt
 		done
 	fi
-	if [ $scen -gt 300 ]
+	if [ $scen -gt 100 ]
 	then
 		for i in 1 2 3
 		do
-		echo "STARTING $val - Trial $i - Base $scen" >> parint_2to6ranks_64.txt 
+		echo "STARTING $val - Trial $i - Base $scen" >> parint_4to6ranks_64.txt 
 		kubectl create -f smpi.yaml
 		sleep $scenarios
 		kubectl create -f scheduler.yaml
-		sleep 900
-		kubectl describe job.batch.volcano.sh >> parint_2to6ranks_64.txt 
+		sleep 400
+		kubectl describe job.batch.volcano.sh >> parint_4to6ranks_64.txt 
 		kubectl delete -f smpi.yaml -f scheduler.yaml
 		sleep 10
-		echo "FINISHED" >> parint_2to6ranks_64.txt
+		echo "FINISHED" >> parint_4to6ranks_64.txt
 		done
 	fi
 	done
