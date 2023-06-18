2dddb0e778f66f83a5918e684c1d7e6b93ad5575
Daniel Medeiros <daniel@kuriate.com.br>
2023-06-19 01:58:51 +0200

    mpi_host

--- stream-mpi/update.sh
+++ stream-mpi/update.sh
@@ -2,7 +2,7 @@
 git add .
 git commit -m "mpi_host"
 git push
-docker rmi raijenki/mpik8s:smpi
+docker rmi raijenki/mpik8s:smpi --no-cache
 docker build . -t raijenki/mpik8s:smpi
 docker push raijenki/mpik8s:smpi
 kubectl create -f smpi.yaml
